94cb8d9755979d98fe8d066836e5ee2efc7782bc
Elijas <master.elijas@gmail.com>
2018-05-12 14:03:21 +0300

    Add comments by the requirement

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import math
 
 
 def writeOutputFile(inputFilename, contents):
-    outputFile = open(inputFilename + "_out.txt", 'w')
+    outputFile = open(inputFilename + "_out.txt", 'w')  # Irasomas tekstas i faila
     outputFile.write(contents)
 
 
 def readInputFile(inputFilename):
     inputFile = open(inputFilename + ".txt", 'r')  # Atidaromas failas skaitymo rezimu
     inputFileContents = inputFile.read()
-    if len(inputFileContents) == 0:
+    if len(inputFileContents) == 0:  # Jei ivesties failas tuscias, programa - stabdoma
         writeOutputFile(inputFilename, "Ivesties failas yra tuscias")
         exit(1)
 
@@ -28,17 +28,17 @@ def binarySearch(where, what, _offsetCounter=0):
         return None
     elif what < centerElement:
         return binarySearch(where=where[:centerIdx], what=what, _offsetCounter=_offsetCounter)
-    return binarySearch(where=where[centerIdx+1:], what=what, _offsetCounter=_offsetCounter + centerIdx + 1)
+    return binarySearch(where=where[centerIdx + 1:], what=what, _offsetCounter=_offsetCounter + centerIdx + 1)
 
 
 if __name__ == "__main__":
     # INPUT
     inputFilename = "ivestis_1000"
-    haystack = readInputFile(inputFilename)
+    haystack = readInputFile(inputFilename)  # Skaitomas failas
     needle = int(input("Kokio skaiciaus ieskote? "))  # Dialogo pagalba suzinoma, kokio skaiciaus ieskoma
 
     # CALCULATION
-    startTime_s = time.clock()
+    startTime_s = time.clock()  # Matuojamas vykdymo laikas
     foundIdx = binarySearch(where=haystack, what=needle)
     duration_us = (time.clock() - startTime_s) * 1e6
 
@@ -49,4 +49,4 @@ if __name__ == "__main__":
         outputContentsTemplate = "Skaicius rastas (indeksuojamoje nuo 0) pozicijoje {} per {} us (=1e-6 s)"
         outputContents = outputContentsTemplate.format(foundIdx, duration_us)
     print(outputContents)
-    writeOutputFile(inputFilename, outputContents)
+    writeOutputFile(inputFilename, outputContents)  # Isvedami rezultatai i faila
